e5c4a755382498cf3dcfdc0e6c80522f2a012dd0
einartech <imalovelyghost13@gmail.com>
2025-05-22 10:20:35 +0200

    fix(SecurityConfig):Add functions required to use the apliccation

--- auditxtoolkit/src/main/java/com/auditxtoolkit/auditxtoolkit/security/SecurityConfig.java
+++ auditxtoolkit/src/main/java/com/auditxtoolkit/auditxtoolkit/security/SecurityConfig.java
@@ -19,6 +19,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
